ebabbe0eccf859045a6589ae6fd1d6e207c042ec
jyneto <j-pm@live.se>
2025-11-30 22:48:47 +0100

    Add AvailableTableDTO class for table data transfer
    Introduced a new namespace `WeddingInvite.Api.DTOs.TableDTO`
    and added the `AvailableTableDTO` class. This class includes
    properties for `TableId`, `TableNumber`, `Capacity`, and
    `AvailableSeats` to encapsulate table-related data. The DTO
    is intended to streamline data transfer between application
    layers, such as the API and client.

---
+++ DTOs/TableDTO/AvailableTableDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace WeddingInvite.Api.DTOs.TableDTO
+{
+    public class AvailableTableDTO
+    {
+        public int TableId { get; set; }
+        public int TableNumber { get; set; }
+        public int Capacity { get; set; }
+        public int AvailableSeats { get; set; }
+
+    }
+ 
+}
+

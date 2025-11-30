16824ca829d907a8087d5e23201d734fa09e3d5c
jyneto <j-pm@live.se>
2025-11-30 22:49:26 +0100

    Add RsvpBookingRequestDTO for RSVP data handling
    Introduced a new namespace `WeddingInvite.Api.DTOs.BookingDTO` and added the `RsvpBookingRequestDTO` class to manage RSVP booking requests.
    
    The class includes properties for personal details (`FullName`, `Email`, `Phone`, `Allergies`), attendance status (`IsAttending`), event-specific details (`TableId`, `StartTime`), and party size (`PartySize`). This enhances the API's ability to handle RSVP data in a structured and consistent manner.

---
+++ DTOs/BookingDTO/RsvpBookingRequestDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace WeddingInvite.Api.DTOs.BookingDTO
+{
+    public class RsvpBookingRequestDTO
+    {
+        public string FullName { get; set; } = string .Empty;
+        public string Email { get; set; } = string.Empty;
+        public string? Phone {  get; set; }
+
+        public string? Allergies { get; set; }
+
+        public bool IsAttending { get; set; }
+
+        public int TableId { get; set; }
+        public DateTime StartTime { get; set; }
+        public int PartySize { get; set; }
+    }
+}
